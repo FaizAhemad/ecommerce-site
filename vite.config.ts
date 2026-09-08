ef6e2c35d4e3ee4476fc0e1fc391e45c4b5c2f0f
FaizAhemad962 <sfaiz57@gmail.com>
2026-09-08 20:08:35 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'force-exit-after-build',
+      apply: 'build',
+      closeBundle() {
+        setTimeout(() => process.exit(0), 0)
+      }
+    }
+  ],
   server: {
     host: '127.0.0.1',
     port: 3000,
